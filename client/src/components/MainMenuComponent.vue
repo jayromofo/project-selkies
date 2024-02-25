0d310f2bbeb4ff8c87d5fbf5d43eb5a5f1182d51
Jason Rossetti <jayromofo@outlook.com>
2024-02-24 21:58:23 -0500

    added: Started working on recipes

--- client/src/components/MainMenuComponent.vue
+++ client/src/components/MainMenuComponent.vue
@@ -1,35 +1,20 @@
 <script setup lang="ts">
 
-import axios from 'axios'
 
-const body = async function testdata(){
-  try {
-    const response = await axios('http://localhost:4444/sample');
-    console.log(response);
-  } catch (error){
-    console.error(error)
-  }
-
-}
 
 </script>
 
 <template>
-  <div>
-    <h2>This is the Main Menu</h2>
+  <div> 
     <p>
       <router-link to="/"></router-link>
       <router-link to="/about">About</router-link>
       <router-link to="/bio">Bio</router-link>
-      <router-link to="/recipes">Recipes</router-link>
-      <span>{{ body.name }}</span>
-      
-      
+      <router-link to="/recipes">Recipes</router-link>         
     </p>
   </div>
   
   <router-view></router-view>
-  <button onclick="testdata()">Testing</button>
 </template>
 
 <style scoped></style>
--- client/src/views/RecipeView.vue
+++ client/src/views/RecipeView.vue
@@ -11,26 +11,67 @@ interface Recipe {
 }
 
 const loading = ref(false);
-const data = ref<Recipe[] | null>(null);
-const error = ref< string | null>(null);
+const data = ref<Recipe[] | null >([]);
+const error = ref< String | null>(null);
+const state = ref <State | null>(null);
+const newRecipe = ref<Recipe>({
+   id: 0,
+   name: '',
+   description: '',
+   type: '',
+   category: '',
+});
 
+defineProps({
+   modelValue: String,
+})
 
+const emit = defineEmits<{
+   (event: 'update:modelValue', payload: String): void;
+}>();
+
+enum State {
+   Create = 1,
+   ViewAll,
+   View,
+   Edit,
+}
 
 const fetchData = async () => {
    loading.value = true;
    try {
       const response = await axios.get('http://localhost:4444/api/recipe/1');
       data.value = response.data;
+
    } catch (err: any) {
       error.value = err.message || 'Error fetching data';
    } finally {
       loading.value = false;
       console.log(data)
    }
-
 }
 
+const addRecipe = () => {
+   console.log('Form Submitted with data: ', newRecipe.value)
+   if (newRecipe.value?.name.trim() != undefined && data.value != undefined) {
+      const newId  = Math.max(...data.value.map((item) => item.id), 0) + 1;
+   }
+}
+
+
+const deleteRecipe = (itemId: number) => {
+   const index = data.value?.findIndex((item) => item.id === itemId)
+   if (index !== -1 && index !== undefined) {
+      data.value?.splice(index, 1);
+   }
+};
+
+
+
+
 onMounted(fetchData);
+state.value = State.Create;
+console.log(state.value);
 
 
 
@@ -41,10 +82,27 @@ console.log(data);
 
 <template>
    <div class="recipe">
-      <p v-if="loading">Loading...</p>
-      <ul v-if="data">
-         <li class="recipe-link" v-for="recipe in data" :key="recipe.id">{{ recipe }}</li>
-      </ul>
+      <div v-if="state == State.Create">
+         <span>This is State Create</span>
+         <form @submit.prevent="addRecipe">
+            <label for="recipeName">Recipe Name: </label>
+            <input type="text" id="recipeName" v-model="newRecipe.name" required /><br />
+            <label for="recipeDescription">Description: </label>
+            <input type="text" id="recipeDescription" v-model="newRecipe.description" required /><br />
+            <label for="recipeType">Recipe Type: </label>
+            <input type="text" id="recipeName" v-model="newRecipe.type" required /><br />
+            <label for="recipeCategory">Recipe Category: </label>
+            <input type="text" id="recipeCategory" v-model="newRecipe.category" required /><br />
+
+            <button type="submit">Add Recipe</button>          
+         </form>
+      </div>
+      <div v-if="state == State.ViewAll">
+         <p v-if="loading">Loading...</p>
+         <ul v-if="data">
+            <li class="recipe-link" v-for="recipe in data" :key="recipe.id">{{ recipe }}</li>
+         </ul>
+      </div>
    </div>
 </template>
  
@@ -56,7 +114,7 @@ console.log(data);
       align-items: center;      
    }
    .recipe-link {
-      font:white;
+      color:white;
    }
 }
 </style>
