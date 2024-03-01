0f03456d3d174f3cd7f9d378ad3c13306f5c3c9a
Jason Rossetti <jayromofo@outlook.com>
2024-03-01 04:43:40 -0500

    cleaned up a bit more

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config' 
-import 'primevue/resources/themes/aura-light-green/theme.css'
+
 
 
 /* Import Modules */
